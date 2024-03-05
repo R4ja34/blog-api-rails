372b743b5bf95040587b973e4e52bdc514512e15
R4ja34 <raja.lalaina@gmail.com>
2024-03-05 01:47:27 +0100

    final-commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,61 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show update destroy ]
+  before_action :authenticate_user!, only: %i[ create update destroy ]
+  before_action :check_article_owner, only: %i[ update destroy ]
+
+
+  # GET /articles
+  def index
+    @articles = Article.order('created_at DESC')
+
+    render json: @articles
+  end
+
+  # GET /articles/1
+  def show
+    render json: @article
+  end
+
+  # POST /articles
+  def create
+    @article = current_user.articles.new(article_params)
+
+    if @article.save
+      render json: @article, status: :created, location: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /articles/1
+  def update
+    if @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /articles/1
+  def destroy
+    @article.destroy!
+    render json: { message: 'Article was successfully destroyed.' }, status: :ok
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+
+    def check_article_owner
+      unless @article.user == current_user
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
+    end
+end
